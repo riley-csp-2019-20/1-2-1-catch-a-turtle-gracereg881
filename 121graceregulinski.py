c2f68b0d3b918e54316d555dd57dc1fc97617392
gracereg881 <55500943+gracereg881@users.noreply.github.com>
2019-10-23 11:35:33 -0400

    Unfinished Part 2

--- 121graceregulinski.py
+++ 121graceregulinski.py
@@ -1,21 +1,49 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
+import random
 
 #-----game configuration---- # define shape, size, color
 shape = "turtle"
-size = 8
+size = 5
 color = "violet"
+score = 0
 
 #-----initialize turtle----- # events
 jack = trtl.Turtle(shape = shape)
 jack.color(color)
 jack.shapesize(size)
+jack.speed(0)
+
+score_writer = trtl.Turtle()
+score_writer.penup()
+score_writer.goto(-370,270)
+font = ("Arial" , 80 , "bold")
+score_writer.write("=)", font = font)
+
 
 #-----game functions-------- #
+def turtle_clicked(x,y):
+    print("jack was clicked!")
+    change_position()
+    score_counter()
+
+def change_position():
+    jack.penup()
+    jack.ht()
+    new_xpos = random.randint(-400,400)
+    new_ypos = random.randint(-300,300)
+    jack.goto(new_xpos, new_ypos)
+    jack.st()
+
+def score_counter():
+    global score
+    score+=1
+    print(score)
 
 
 #-----events----------------
+jack.onclick(turtle_clicked)
 
 wn=trtl.Screen()
 wn.mainloop()
